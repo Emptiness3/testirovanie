c5b30e424694bff5a6c1e7dbf3f00d80531e508c
Emptiness3 <82176305+Emptiness3@users.noreply.github.com>
2025-04-01 14:07:45 +0500

    1234

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,8 @@
 import pytest
-from calculator import Calculator
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.calculator import Calculator
 
 calc = Calculator()
 
